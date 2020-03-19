5dccd7384e03a27285ab55da2d5fb6147d4d05d6
manaut <manaut.bertonboitel@mmibordeaux.com>
2020-03-19 13:25:25 +0100

    Login system complete

--- Source/login.php
+++ Source/login.php
@@ -1,36 +1,34 @@
+<?php
+require "headersession.php";
+?>
+
 <!DOCTYPE html>
-
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Se connecter</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  </head>
+  <?php
+  require "head.php";
+  ?>
   <body>
-
     <div class="container">
       <?php
       require "nav.php";
       ?>
-
       <div class="row">
-        <form action="php/signin.php" class="col-md-7">
-          <div class="form-group row align-items-center my-4">
-            <label class="col" for="exampleInputEmail1">Adresse Email</label>
-            <input class="col form-control" id="exampleInputEmail1" placeholder="Entrer votre addresse email" name="uname" required>
+        <form action="functions/login.fc.php" class="col-xl-5" method="post">
+          <input  type="text" class="form-control my-3" placeholder="Nom d'utilisateur" name="uname">
+          <input  type="password" class="form-control my-3" placeholder="Mot de passe" name="pwd">
+          <div class="row">
+            <div class="col">
+              <button type="submit" name="login-submit" class="btn btn-primary">Se connecter</button>
+            </div>
+            <div class="col">
+              <a href="signup.php" class="btn btn-link">S'enregistrer</a>
+            </div>
           </div>
-          <div class="form-group row align-items-center">
-            <label class="col" for="exampleInputPassword1">Mot de passe</label>
-            <input type="password" class="col form-control" id="exampleInputPassword1" placeholder="Entrer votre mot de passe" name="psw" required>
-          </div>
-          <button type="submit" name="submit" value="OK" class="btn btn-primary">Se connecter</button>
         </form>
       </div>
     </div>
-
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <?php
+    require "scripts.php";
+    ?>
   </body>
 </html>
--- Source/nav.php
+++ Source/nav.php
@@ -3,15 +3,24 @@
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
   <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" href="index.php">Accueil</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="signup.php">S'enregistrer</a>
-      </li>
+      <?php
+        if (isset($_SESSION['userId'])) {
+            echo '<li class="nav-item ml-2"><a href="functions/logout.fc.php" name class="btn btn-outline-danger">Se déconnecter</a></li>';
+        }
+        else {
+            echo "<li class='nav-item'>
+                    <a href='signup.php' class='btn btn-outline-primary'>S'enregistrer</a>
+                  </li>
+                  <li class='nav-item ml-2'>
+                    <a href='login.php' class='btn btn-outline-primary'>Se connecter</a>
+                  </li>";
+        }
+      ?>
     </ul>
   </div>
 </nav>
